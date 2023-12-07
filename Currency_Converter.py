6390f1335adb2a43551213ca5216a1f8ffa5364b
Mohan5353 <120332828+Mohan5353@users.noreply.github.com>
2023-12-08 00:50:08 +0530

    Update Currency_Converter.py

--- Currency_Converter.py
+++ Currency_Converter.py
@@ -13,12 +13,12 @@ def update(*args: object) -> dict:
     present = date.today().strftime("%d %b %Y")
     response = get_response()
     file_date = response["time_last_update_utc"][5:16]
-    if present != file_date:
-        f = open('Currency_Rate.json', 'w')
-        url = "https://v6.exchangerate-api.com/v6/a04cc39aea88ee70a55c180c/latest/USD"
-        response = get(url).json()
-        dump(response, f, indent=4)
-        f.close()
+    # if present != file_date:
+    #     f = open('Currency_Rate.json', 'w')
+    #     url = "https://v6.exchangerate-api.com/v6/a04cc39aea88ee70a55c180c/latest/USD"
+    #     response = get(url).json()
+    #     dump(response, f, indent=4)
+    #     f.close()
     response = response['conversion_rates']
     return response
 
