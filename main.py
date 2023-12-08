cc771f7d4a381ecc6fc20bb4871d34c612ccfc00
Mohan5353 <120332828+Mohan5353@users.noreply.github.com>
2023-12-08 18:09:56 +0530

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import flet as ft
 
-import Currency_Converter as cc  # noqa n813
+from datetime import date
+from json import dump, load
+
+from requests import get
+
+
+def get_response(*args) -> dict:
+    with open('Currency_Rate.json') as f:
+        return load(f)
+
+
+def update(*args: object) -> dict:
+    present = date.today().strftime("%d %b %Y")
+    response = get_response()
+    file_date = response["time_last_update_utc"][5:16]
+    # if present != file_date:
+    #     f = open('Currency_Rate.json', 'w')
+    #     url = "https://v6.exchangerate-api.com/v6/a04cc39aea88ee70a55c180c/latest/USD"
+    #     response = get(url).json()
+    #     dump(response, f, indent=4)
+    #     f.close()
+    response = response['conversion_rates']
+    return response
 
 
 def main(page: ft.Page) -> None:
-    data = cc.update()
+    data = update()
     currency = sorted(data.keys())
     page.title = "Currency Converter"
     page.window_width = 400
